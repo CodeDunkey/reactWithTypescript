ae26539adf41670da0c772f04faa8968392153e0
Tastan Kodersen <andmarknu@hotmail.com>
2023-12-07 22:08:54 +0100

    07.12.20.23 22:08

--- react-with-ts/src/Types/Transactions.tsx
+++ react-with-ts/src/Types/Transactions.tsx
@@ -35,24 +35,24 @@ interface TestPerson1 {
     cars: string[]
 }
 
-interface TestPerson2 {}
+interface TestPerson2 { }
 
 let typeUpOfTest: TestPerson1 = {
     name: "John",
     middle_name: null,
     age: 100,
     alive: true,
-    addresses: {somewhere1: "here", somewhere2:"there", somewhere3: "everywhere"},
-    cars: ["Ford", "Skoda", "VW" ]
+    addresses: { somewhere1: "here", somewhere2: "there", somewhere3: "everywhere" },
+    cars: ["Ford", "Skoda", "VW"]
 }
 
 console.log(typeUpOfTest);
 
 // console.log(test);
 
-for (let key in test.testPerson1.addreses){
+for (let key in test.testPerson1.addreses) {
     // console.log("key", key);
-    if (test.testPerson1.addreses.hasOwnProperty(key)){
+    if (test.testPerson1.addreses.hasOwnProperty(key)) {
         // console.log(`${key}: ${test.testPerson1.addreses[key]}`) //[key]
     }
 };
@@ -63,14 +63,14 @@ const population = {
     male: 4,
     female: 93,
     others: 10
-  };
-  
-  // Iterate through the object
-  for (let key in population) {
+};
+
+// Iterate through the object
+for (let key in population) {
     if (population.hasOwnProperty(key)) {
-    //   console.log(`${key}: ${population[key]}`);
+        //   console.log(`${key}: ${population[key]}`);
     }
-  }
+}
 
 
 
@@ -90,6 +90,97 @@ const population = {
 
 // console.log("parse from testParse1", testParse1);
 
-export default function Transaction(){
-    
+export default function Transaction() {
+
 }
+
+{
+    "transactionList": [
+        {
+            // "accountCombinedKey": "e2NsZWFyaW5nOiI5OTYxIixhY2NvdW50OiI5OTYxMDk0OTgwNDY0MDIiLGFncmVlbWVudDoiOTk2MTc3MjY1MDE1ODI1Iix1c2VyOiI5OTYxOTUwMjM4OCJ9",
+            // "transactionCombinedKey": "e2FjY291bnQ6Ijk5NjEwOTQ5ODA0NjQwMiIsYWdyZWVtZW50OiI5OTYxNzcyNjUwMTU4MjUiLHRyYW5zYWN0aW9uaWQ6Ijk5NjEwOTQ5ODA0NjQwMkAwQTEwRUZCRkAyMDIzLTA5LTA4QDIwMjMtMDktMDgtMTEuNTQuNTYuODU1NTAyIix1c2VyOiI5OTYxOTUwMjM4OCJ9",
+            // "bookingDate": "2023-09-08",
+            // "valueDate": "2023-09-08",
+            // "originalDate": "2023-09-08",
+            // "availableDate": "2023-09-08",
+            // "transactionText": "Overførsel",
+            // "individualText": "",
+            // "customizedText": "",
+            // "statementText": "Overførsel",
+            // "postingConditionRelationshipText": "",
+            // "transactionAmount": {
+            //     "amount": {
+            //         "decimalValue": 1.00,
+            //         "integerValue": 100,
+            //         "noOfDecimals": 2
+            //     },
+            //     "currencyCode": "DKK"
+            // },
+            // "originalAmount": {
+            //     "amount": {
+            //         "decimalValue": 1.00,
+            //         "integerValue": 100,
+            //         "noOfDecimals": 2
+            //     },
+            //     "currencyCode": "DKK"
+            // },
+            "transactionType": "aicl",
+            "corebankTimestamp": "2023-09-08T09:54:56.855502+00:00",
+            "balance": {
+                "decimalValue": 466403.73,
+                "integerValue": 46640373,
+                "noOfDecimals": 2
+            },
+            "isReconciled": false,
+            "isCombinedPosting": false,
+            "sortingKey": "99999999",
+            "senderInfo": {
+                "senderAddress1": "Homebankingbruger Fr.",
+                "senderAddress2": "Vivian Lund",
+                "senderAddress3": "Borupvang 3",
+                "senderAddress4": "2750 Ballerup",
+                "senderAddress5": "",
+                "senderMessage": "",
+                "isSenderMessageCutOff": false
+            }
+        }
+    ]
+}
+
+interface amount2 {
+        decimalValue: number,
+        integerValue: number,
+        noOfDecimals: number
+    }
+
+interface originalAmount extends amount2{
+    currencyCode: string
+}
+
+interface amount1 {
+    decimalValue: number,
+    integerValue: number,
+    noOfDecimals: number
+}
+
+interface transactionAmount extends amount1{       
+    currencyCode: string
+}
+
+interface ATransaction extends transactionAmount{
+    accountCombinedKey: string,
+    transactionCombinedKey: string,
+    bookingDate: string,
+    valueDate: string,
+    originalDate: string,
+    availableDate: string,
+    transactionText: string,
+    individualText?: string,
+    customizedText?: string,
+    statementText: string,
+    postingConditionRelationshipText?: string,
+
+}
+
+interface ATransactionList { transaction: ATransaction[]};
+
