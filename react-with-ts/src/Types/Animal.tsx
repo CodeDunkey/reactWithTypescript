622e75081d73e2bedac4ca44f1f5b1b74b2db6a3
Andreas <andmarknu@hotmail.com>
2023-12-08 15:23:57 +0100

    08.12.2023 15:23

--- react-with-ts/src/Types/Animal.tsx
+++ react-with-ts/src/Types/Animal.tsx
@@ -49,21 +49,46 @@ let testPerson_2: TestPerson_2 = {
 type Choose_A_Person = TestPerson_1 | TestPerson_2;
 
 const isPerson_1 = (parameter: Choose_A_Person): parameter is TestPerson_1 =>{
-    // console.log("parameter as TestPerson_1", parameter as TestPerson_1)
+    let age = (parameter as TestPerson_1).age;
+    // console.log(age);
+    if(typeof age === "number") {
+        console.log("age times two: ",(age as number).valueOf()*2)
+    }
+    else console.log("'age' Must be a number!")
+    
     return ((parameter as TestPerson_1).age !== undefined)
 } 
+
 // console.log("isPerson_1(testPerson_2)", isPerson_1(testPerson_2))
+
 const isPerson_2 = (parameter: Choose_A_Person): parameter is TestPerson_2 =>{
     return ((parameter as TestPerson_2).employee_id !== undefined)
 }
 
 
-function showPerson (param: Choose_A_Person){
-    // console.log(param)
-    if(isPerson_1(param))console.log(param.addresses)
-    if(isPerson_2(param))console.log(param.addresses)
+const showPerson = (param: Choose_A_Person) => {
+    if(isPerson_1(param))console.log(param.cars)
+    if(isPerson_2(param))console.log(param.projects)
 }
-showPerson(testPerson_2);
+showPerson(testPerson_1);
+
+type Input = unknown;
+
+let input: Input;
+
+const inputCheck = (param: Input) => {
+    if(typeof param === "string") {
+        console.log((param as string).toUpperCase())
+    }
+    else if(typeof param === "number") {
+        console.log((param as number).valueOf()*2)
+    }
+    else console.log("not a valid 'type'")
+
+
+}
+
+// inputCheck(4)
 export default function Animal(){
    
         
