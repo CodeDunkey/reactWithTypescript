b4ea20ebeca4b52fa8edf5bb669861884d053986
Tastan Kodersen <andmarknu@hotmail.com>
2023-12-08 06:55:33 +0100

    08.12.20.23 06:55

--- react-with-ts/src/Types/Transactions.tsx
+++ react-with-ts/src/Types/Transactions.tsx
@@ -94,57 +94,73 @@ export default function Transaction() {
 
 }
 
-{
-    "transactionList": [
-        {
-            // "accountCombinedKey": "e2NsZWFyaW5nOiI5OTYxIixhY2NvdW50OiI5OTYxMDk0OTgwNDY0MDIiLGFncmVlbWVudDoiOTk2MTc3MjY1MDE1ODI1Iix1c2VyOiI5OTYxOTUwMjM4OCJ9",
-            // "transactionCombinedKey": "e2FjY291bnQ6Ijk5NjEwOTQ5ODA0NjQwMiIsYWdyZWVtZW50OiI5OTYxNzcyNjUwMTU4MjUiLHRyYW5zYWN0aW9uaWQ6Ijk5NjEwOTQ5ODA0NjQwMkAwQTEwRUZCRkAyMDIzLTA5LTA4QDIwMjMtMDktMDgtMTEuNTQuNTYuODU1NTAyIix1c2VyOiI5OTYxOTUwMjM4OCJ9",
-            // "bookingDate": "2023-09-08",
-            // "valueDate": "2023-09-08",
-            // "originalDate": "2023-09-08",
-            // "availableDate": "2023-09-08",
-            // "transactionText": "Overførsel",
-            // "individualText": "",
-            // "customizedText": "",
-            // "statementText": "Overførsel",
-            // "postingConditionRelationshipText": "",
-            // "transactionAmount": {
-            //     "amount": {
-            //         "decimalValue": 1.00,
-            //         "integerValue": 100,
-            //         "noOfDecimals": 2
-            //     },
-            //     "currencyCode": "DKK"
-            // },
-            // "originalAmount": {
-            //     "amount": {
-            //         "decimalValue": 1.00,
-            //         "integerValue": 100,
-            //         "noOfDecimals": 2
-            //     },
-            //     "currencyCode": "DKK"
-            // },
-            "transactionType": "aicl",
-            "corebankTimestamp": "2023-09-08T09:54:56.855502+00:00",
-            "balance": {
-                "decimalValue": 466403.73,
-                "integerValue": 46640373,
-                "noOfDecimals": 2
-            },
-            "isReconciled": false,
-            "isCombinedPosting": false,
-            "sortingKey": "99999999",
-            "senderInfo": {
-                "senderAddress1": "Homebankingbruger Fr.",
-                "senderAddress2": "Vivian Lund",
-                "senderAddress3": "Borupvang 3",
-                "senderAddress4": "2750 Ballerup",
-                "senderAddress5": "",
-                "senderMessage": "",
-                "isSenderMessageCutOff": false
-            }
-        }
-    ]
+// {
+//     "transactionList": [
+//         {
+//             // "accountCombinedKey": "e2NsZWFyaW5nOiI5OTYxIixhY2NvdW50OiI5OTYxMDk0OTgwNDY0MDIiLGFncmVlbWVudDoiOTk2MTc3MjY1MDE1ODI1Iix1c2VyOiI5OTYxOTUwMjM4OCJ9",
+//             // "transactionCombinedKey": "e2FjY291bnQ6Ijk5NjEwOTQ5ODA0NjQwMiIsYWdyZWVtZW50OiI5OTYxNzcyNjUwMTU4MjUiLHRyYW5zYWN0aW9uaWQ6Ijk5NjEwOTQ5ODA0NjQwMkAwQTEwRUZCRkAyMDIzLTA5LTA4QDIwMjMtMDktMDgtMTEuNTQuNTYuODU1NTAyIix1c2VyOiI5OTYxOTUwMjM4OCJ9",
+//             // "bookingDate": "2023-09-08",
+//             // "valueDate": "2023-09-08",
+//             // "originalDate": "2023-09-08",
+//             // "availableDate": "2023-09-08",
+//             // "transactionText": "Overførsel",
+//             // "individualText": "",
+//             // "customizedText": "",
+//             // "statementText": "Overførsel",
+//             // "postingConditionRelationshipText": "",
+//             // "transactionAmount": {
+//             //     "amount": {
+//             //         "decimalValue": 1.00,
+//             //         "integerValue": 100,
+//             //         "noOfDecimals": 2
+//             //     },
+//             //     "currencyCode": "DKK"
+//             // },
+//             // "originalAmount": {
+//             //     "amount": {
+//             //         "decimalValue": 1.00,
+//             //         "integerValue": 100,
+//             //         "noOfDecimals": 2
+//             //     },
+//             //     "currencyCode": "DKK"
+//             // },
+//             // "transactionType": "aicl",
+//             // "corebankTimestamp": "2023-09-08T09:54:56.855502+00:00",
+//             // "balance": {
+//             //     "decimalValue": 466403.73,
+//             //     "integerValue": 46640373,
+//             //     "noOfDecimals": 2
+//             // },
+//             // "isReconciled": false,
+//             // "isCombinedPosting": false,
+//             // "sortingKey": "99999999",
+//             // "senderInfo": {
+//             //     "senderAddress1": "Homebankingbruger Fr.",
+//             //     "senderAddress2": "Vivian Lund",
+//             //     "senderAddress3": "Borupvang 3",
+//             //     "senderAddress4": "2750 Ballerup",
+//             //     "senderAddress5": "",
+//             //     "senderMessage": "",
+//             //     "isSenderMessageCutOff": false
+//             // }
+//         }
+//     ]
+// }
+
+interface SenderInfo {
+    senderAddress1: string,
+    senderAddress2: string,
+    senderAddress3: string,
+    senderAddress4: string,
+    senderAddress5?: string,
+    senderMessage?: string,
+    isSenderMessageCutOff: boolean
+}
+
+interface Balance {
+    decimalValue: number,
+    integerValue: number,
+    noOfDecimals: number
 }
 
 interface amount2 {
@@ -167,7 +183,7 @@ interface transactionAmount extends amount1{
     currencyCode: string
 }
 
-interface ATransaction extends transactionAmount{
+interface ATransaction extends transactionAmount, originalAmount, Balance, SenderInfo{
     accountCombinedKey: string,
     transactionCombinedKey: string,
     bookingDate: string,
@@ -180,6 +196,12 @@ interface ATransaction extends transactionAmount{
     statementText: string,
     postingConditionRelationshipText?: string,
 
+    transactionType: string,
+    corebankTimestamp: string,
+
+    isReconciled: boolean,
+    isCombinedPosting: boolean,
+    sortingKey: number,
 }
 
 interface ATransactionList { transaction: ATransaction[]};
